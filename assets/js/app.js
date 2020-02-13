132919198b2b3bef2da4e631173ec80947547a77
gavani <intalmaci21@gmail.com>
2020-02-13 12:09:38 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.header__navbar-menu-icon').click(function(){
+		$(this).toggleClass('open');
+		$('.header__menu').fadeToggle(400);
+		$('.section').toggleClass('blur');
+		$('body').toggleClass('menu_open');
+	});
+	
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcblx0JCgnLmhlYWRlcl9fbmF2YmFyLW1lbnUtaWNvbicpLmNsaWNrKGZ1bmN0aW9uKCl7XHJcblx0XHQkKHRoaXMpLnRvZ2dsZUNsYXNzKCdvcGVuJyk7XHJcblx0XHQkKCcuaGVhZGVyX19tZW51JykuZmFkZVRvZ2dsZSg0MDApO1xyXG5cdFx0JCgnLnNlY3Rpb24nKS50b2dnbGVDbGFzcygnYmx1cicpO1xyXG5cdFx0JCgnYm9keScpLnRvZ2dsZUNsYXNzKCdtZW51X29wZW4nKTtcclxuXHR9KTtcclxuXHRcclxufSk7Il19
